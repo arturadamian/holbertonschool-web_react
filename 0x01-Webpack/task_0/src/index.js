c71c598c94f5f73099302fc15299b4381cd70f01
arturadamian <arturadamian@gmail.com>
2020-08-14 20:40:06 -0700

    initial commit

---
+++ 0x01-Webpack/task_0/src/index.js
@@ -0,0 +1,6 @@
+import $ from "jquery";
+
+$("body").add("<p>Holberton Dashboard</p><br>\
+               <p>Dashboard data for the students</p><br>\
+               <p>Dashboard data for the students</p>");
+               
