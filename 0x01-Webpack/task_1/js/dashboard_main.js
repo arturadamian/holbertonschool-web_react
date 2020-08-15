8ed11564a9830d798ca6df9113b1abe07f25be36
arturadamian <arturadamian@gmail.com>
2020-08-14 22:32:26 -0700

    initial commit

---
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+function updateCounter() {
+  let counter = 0;
+  $('button').on('click', () => {
+    counter++;
+    console.log(counter);
+    $('#count').html(`${counter} clicks on the button`);
+  });
+};
+(() => {
+  let dprocess = $.debounce(updateCounter, 5000);
+  dprocess();
+});
+updateCounter();
