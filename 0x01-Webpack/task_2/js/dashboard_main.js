4ce8ff39e75aa67d256f9da8b8d8fcb178055f0b
arturadamian <arturadamian@gmail.com>
2020-08-25 22:23:47 -0700

    correct the import in files

--- 0x01-Webpack/task_2/js/dashboard_main.js
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
+import _ from 'lodash';
+import '../css/main.css';
 
-$('body').prepend('<span id="logo"></span>');
+$('body').append('<div id="logo"></div>');
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
@@ -8,15 +10,12 @@ $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
 function updateCounter() {
-  let counter = 0;
+  let counter = $('#count').html() || 0;
   $('button').on('click', () => {
-    counter++;
-    console.log(counter);
+    counter += 1;
     $('#count').html(`${counter} clicks on the button`);
   });
 };
-(() => {
-  let dprocess = $.debounce(updateCounter, 5000);
-  dprocess();
-});
+
+_.debounce(updateCounter, 500);
 updateCounter();
