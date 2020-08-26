114f512bc369a81d110d8923363dcca85a4a7e0a
arturadamian <arturadamian@gmail.com>
2020-08-25 23:18:43 -0700

    add performance to webconfig

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -2,10 +2,13 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry : "./js/dashboard_main.js",
+    entry : path.resolve(__dirname, 'js/dashboard_main.js'),
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
+    },
+    performance: {
+      maxAssetSize: 1000000
     },
     module: {
       rules: [
